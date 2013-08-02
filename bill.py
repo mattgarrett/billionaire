b62e1c7c49701089d6545a661cfd3274eef3cfdf
matt <matthew.e.garrett@gmail.com>
2013-08-02 11:48:46 -0700

    added a limit to the number of trials

--- bill.py
+++ bill.py
@@ -6,6 +6,17 @@
 
 import csv
 
+#max number of days the trial will run
+#0 signals no max
+limit = 100
+
+count = 0
+history = []
 cr = csv.reader(open("nasdaq.csv", "r"))
+
 for row in cr:
-    print row
+    count = count + 1
+    if count == limit:
+        break
+    history.append(row[4])
+    print history
