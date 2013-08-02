9c1c92e728eb30df837b2aace92b7d28460b0897
matt <matthew.e.garrett@gmail.com>
2013-08-02 16:08:35 -0700

    bill.py should now ask the given rule file for its moves on every turn

--- bill.py
+++ bill.py
@@ -1,4 +1,4 @@
-#/usr/bin/python
+#!/usr/bin/python3
 
 #bill.py
 #
@@ -8,35 +8,70 @@ import csv
 import sys
 
 if len(sys.argv) == 1:
-    print 'must provide a python file name as the first argument'
-    print 'this file will be treated as the rules file'
+    print ('must provide a python file name as the first argument')
+    print ('this file will be treated as the rules file')
     sys.exit(0)
 
 try:
-    print (sys.argv[1])
-    rules = __import__((sys.argv[1])[0:-3])
+    rule = __import__((sys.argv[1])[0:-3])
 except:
-    print 'couldn\'t import the rules file: '
-    print sys.argv[1]
+    print ('couldn\'t import the rules file:')
+    print (sys.argv[1])
     sys.exit(0)
 
 #max number of days the trial will run
-#0 signals no max
+#-1 signals no max
 limit = -1
 if len(sys.argv) > 2:
     try:
         limit = int(sys.argv[2])
     except:
-        print 'second command line argument must be an int'
+        print ('second command line argument must be an int')
         sys.exit(0)
 
 count = 0
 history = []
+#TODO: unhardcode the file name
 cr = csv.reader(open("nasdaq.csv", "r"))
+
+#initial value of stock, initial value of capital
+stock = 10000
+cash = 10000
+
+#just leave this at zero, because it won't
+#as rules for a decision until there is some history
+order = 0
 
 for row in cr:
     if count == limit:
         break
-    history.append(row[4])
-    rules.main(history)
+    history.append(float(row[4]))
+    if len(history) > 10:
+        today = len(history) - 1
+
+        old = stock + cash
+
+        #the change in the market
+        change = ((history[today] - history[today - 1]) / history[today - 1])
+        stock = stock + (stock * change)
+        
+        if ((cash + stock) >= old):
+            print ('day ' + str(count) + ': ' + str(stock + cash) + ', +' + str(stock + cash - old))
+        else:
+            print ('day ' + str(count) + ': ' + str(stock + cash) + ', ' + str(stock + cash - old))
+
+        order = rule.main(history, stock, cash)
+        if (order < 0 and 0 - order > stock):
+            print ('can\'t sell more stock than you have')
+            print ('sell order: ' + str(order) + ' in stock')
+            print ('you have  : ' + str(stock) + ' in stock')
+            sys.exit(1)
+        if (order > 0 and order > cash):
+            print ('can\'t spend more money than you have')
+            print ('buy order : ' + str(order) + ' in cash')
+            print ('you have  : ' + str(cash)  + ' in cash')
+            sys.exit(1)
+
+        stock = stock + order
+        cash = cash - order
     count = count + 1
--- matt.py
+++ matt.py
@@ -1,7 +1,16 @@
 import sys
 
-def main(argv):
-    print (argv)
+def main(argv, stock, cash):
+    if len(argv) < 1:
+        return 0
+    else:
+        today = len(argv) - 1
+        if (argv[today] > argv[today - 1]):
+            return 0
+        elif (argv[today] < argv[today - 1]):
+            return 0
+        else:
+            return 0
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main(sys.argv, stock, cash)
