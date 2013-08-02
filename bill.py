61f9564d9dd4e214b41027e6cfed66feec7f8c63
matt <matthew.e.garrett@gmail.com>
2013-08-02 12:39:42 -0700

    now calls the main function in the specified argument, passing the history array

--- bill.py
+++ bill.py
@@ -6,16 +6,28 @@
 
 import csv
 import sys
-import rules.py
+
+if len(sys.argv) == 1:
+    print 'must provide a python file name as the first argument'
+    print 'this file will be treated as the rules file'
+    sys.exit(0)
+
+try:
+    print (sys.argv[1])
+    rules = __import__((sys.argv[1])[0:-3])
+except:
+    print 'couldn\'t import the rules file: '
+    print sys.argv[1]
+    sys.exit(0)
 
 #max number of days the trial will run
 #0 signals no max
 limit = -1
-if len(sys.argv) > 1:
+if len(sys.argv) > 2:
     try:
-        limit = int(sys.argv[1])
+        limit = int(sys.argv[2])
     except:
-        print 'command line argument must be an int'
+        print 'second command line argument must be an int'
         sys.exit(0)
 
 count = 0
@@ -26,5 +38,5 @@ for row in cr:
     if count == limit:
         break
     history.append(row[4])
-    rules(history)
+    rules.main(history)
     count = count + 1
