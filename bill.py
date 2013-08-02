93d75411df715afaffc349cbdf04a0f7b2aa70de
matt <matthew.e.garrett@gmail.com>
2013-08-02 12:03:46 -0700

    made the day limit a command line argument and will read form rules.py

--- bill.py
+++ bill.py
@@ -5,18 +5,26 @@
 #matt and peter, becoming billionaires
 
 import csv
+import sys
+import rules.py
 
 #max number of days the trial will run
 #0 signals no max
-limit = 100
+limit = -1
+if len(sys.argv) > 1:
+    try:
+        limit = int(sys.argv[1])
+    except:
+        print 'command line argument must be an int'
+        sys.exit(0)
 
 count = 0
 history = []
 cr = csv.reader(open("nasdaq.csv", "r"))
 
 for row in cr:
-    count = count + 1
     if count == limit:
         break
     history.append(row[4])
-    print history
+    rules(history)
+    count = count + 1
