c34d38b50baea9e23ea348b285d8a585d68fdd02
matt <matthew.e.garrett@gmail.com>
2013-09-11 21:07:20 -0700

    added more pretty printing, removed some commented out code

--- bill.py
+++ bill.py
@@ -81,13 +81,6 @@ for row in stockData:
         #the change in the market
         change = ((history[today] - history[today - 1]) / history[today - 1])
         stock[i] = stock[i] + (stock[i] * change)
-        
-        """
-        if ((cash[i] + stock[i]) >= old):
-            print ('day ' + str(count) + ': ' + str(stock + cash) + ', +' + str(stock + cash - old))
-        else:
-            print ('day ' + str(count) + ': ' + str(stock + cash) + ', ' + str(stock + cash - old))
-        """
 
         order = rules[i].main(history, stock[i], cash[i])
         if (order < 0 and 0 - order > stock[i]):
@@ -107,51 +100,22 @@ for row in stockData:
         
         stock[i] = stock[i] + order
         cash[i] = cash[i] - order
-        line = line + pad(str(stock[i] + cash[i]))
+        line = line + pad("$" + str(stock[i] + cash[i]))
 
     count = count + 1
-    print (line, end="\r")
-print (line)
+    print ("\r" + line, end="")
+print ()
 
-"""
-leaderboard = []
-leaderboardNames = []
-words = []
-
-#reads in the leaderboard for comparing results
-leaderboardData = csv.reader(open("data/leaderboard/leaderboard.csv", "r"))
-for row in leaderboardData:
-    leaderboard.append((int(row[0]), row[1], row[2]))
-    leaderboardNames.append(row[2][0:-3])
-
-#reads in words for leaderboard names
-wordsData = csv.reader(open("data/leaderboard/words.csv", "r"))
-for row in wordsData:
-    words.append(row[0])
-
-words = list(filter(lambda word: word not in leaderboardNames, words))
-
-name = choice(words)
-leaderboard.append((round(stock + cash), sys.argv[1], name + ".py"))
-leaderboard = sorted(leaderboard, reverse=True)
-
-print ("\nLeaderboard:")
-for item in leaderboard:
-    print ("   " + str(item))
-
-if len(leaderboard) < 11 or leaderboard[10][2] != name + ".py":
-    copyfile(sys.argv[1], "data/leaderboard/" + name + ".py")
-    call(["git", "add", "data/leaderboard/" + name + ".py"])
-    print ("\nYou made it on the leaderboard as " + name + ".py!")
-    print ("Saved your file at data/leaderboard/" + name + ".py")
-    
-    os.remove("data/leaderboard/leaderboard.csv")
-    with open("data/leaderboard/leaderboard.csv", "w", newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        for i in range(0, min(len(leaderboard), 10)):
-            writer.writerow(leaderboard[i])
-    call(["git", "add", "data/leaderboard/leaderboard.csv"])
-else:
-    print ("You didn't make it on the leaderboard")
-    print ("You must be pretty bad")
-"""
+winner = -1
+value = -1
+for i in range(0, len(rules)):
+    if stock[i] + cash[i] > value:
+        winner = i
+        value = stock[i] + cash[i]
+print (pad(""), end="")
+for i in range(0, len(rules)):
+    if i == winner:
+        print (pad("^ winner ^"), end="")
+    else:
+        print (pad(""), end="")
+print ()
